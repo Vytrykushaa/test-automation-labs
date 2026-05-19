b3399f9db28ebbf383dca08b623f90b7970ceaaa
Anastasia Vytrykush <anastasiia.vytrykush-po221@nung.edu.ua>
2026-05-19 23:22:57 +0300

    Remove Lab8 folder

--- Lab8/cypress-tests/cypress/support/e2e.js
+++
@@ -1,6 +0,0 @@
-// cypress/support/e2e.js
-// Глобальна обробка uncaught exception від реклами Google
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // Повертаємо false — Cypress не завалює тест через помилки сторонніх скриптів
-  return false;
-});
